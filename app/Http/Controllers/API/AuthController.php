45d3c4762ee78add68a19a3d30e28fbc8e43e5d1
bmnaufaal <bmuhammadopang@gmail.com>
2022-03-20 18:53:13 +0700

    Menambah fitur login dan logout

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,12 +6,56 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $user = User::where('email', $request->email)->first();
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return $validator->messages();
+        } else {
+            $login = Auth::attempt(
+                [
+                    'email' => $request->email,
+                    'password' => $request->password
+                ]
+            );
+            if ($login) {
+                $user = User::where('email', $request->email)->first();
+                $tokenResult = $user->createToken('token-auth')->plainTextToken;
+                return response()->json([
+                    'status' => 'Success',
+                    'message' => 'Berhasil login',
+                    'content' => [
+                        'status_code' => 200,
+                        'access_token' => $tokenResult,
+                        'token_type' => 'Bearer'
+                    ]
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'Error',
+                    'message' => 'Username atau password salah'
+                ]);
+            }
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->currentAcccessToken()->delete();
+        return response()->json([
+            'status' => 'Successs',
+            'message' => 'Berhasil logout'
+        ], 200);
     }
 
     public function register(Request $request)
@@ -29,7 +73,7 @@ class AuthController extends Controller
             $data = array(
                 'name' => $request->name,
                 'email' => $request->email,
-                'password' => bcrypt($request->password1)
+                'password' => Hash::make($request->password1)
             );
 
             $register = User::create($data);
